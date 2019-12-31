52a62fc69eb44c8006ea28ef7cdffcbda099fe7e
TechnicallyNeon <33362229+TechnicallyNeon@users.noreply.github.com>
2019-12-31 00:01:46 -0600

    Cleaned up files for rework

---
+++ src/card/BackgroundUtil.java
@@ -0,0 +1,22 @@
+package card;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+
+import javax.imageio.ImageIO;
+
+public class BackgroundUtil
+{
+	private BackgroundUtil()
+	{
+	}
+	
+	public void create(Type t)
+	{
+		switch (t)
+		{
+			case BUG:
+				
+		}
+	}
+}
---
+++ src/card/Card.java
@@ -0,0 +1,21 @@
+package card;
+
+import java.awt.image.BufferedImage;
+
+public class Card
+{
+	/**
+	 * BufferedImage for the cards background color.
+	 */
+	private BufferedImage background;
+	
+	public Card()
+	{
+		
+	}
+	
+	public void drawCard()
+	{
+		
+	}
+}
