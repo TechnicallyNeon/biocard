9d07961f6272886408ea000c11dea9cdc77633d9
TechnicallyNeon <33362229+TechnicallyNeon@users.noreply.github.com>
2020-01-02 19:20:58 -0600

    Added additional functionality to enum Type

--- src/card/Type.java
+++ src/card/Type.java
@@ -1,7 +1,34 @@
 package card;
 
 public enum Type
-{
-	NORMAL, FIRE, FIGHTING, WATER, FLYING, GRASS, POISON, ELECTRIC,	GROUND, 
-	PSYCHIC, ROCK, ICE, BUG, DRAGON, GHOST, DARK, STEEL, FAIRY, NULL
+{	
+	/*
+	 * List of all potential types with respective strings given
+	 */
+	NORMAL("normal"), FIRE("fire"), FIGHTING("fighting"), WATER("water"), FLYING("flying"), GRASS("grass"), POISON("posion"), ELECTRIC("electric"),	
+	GROUND("ground"), PSYCHIC("psychic"), ROCK("rock"), ICE("ice"), BUG("bug"), DRAGON("dragon"), GHOST("ghost"), DARK("dark"), STEEL("steel"), 
+	FAIRY("fairy"), NULL("normal");
+	
+	/**
+	 * String form of Type used to determine how the card should be colored
+	 */
+	private String typeName;
+	
+	/**
+	 * Creates a Type enum with a String determining color usage for the card
+	 * @param name - name of the types color set to be used
+	 */
+	private Type(String name)
+	{
+		typeName = name;
+	}
+	
+	/**
+	 * Returns the color sets type name
+	 * @return a name for the related to the Type specified
+	 */
+	public String getType()
+	{
+		return typeName;
+	}
 }
